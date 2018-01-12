49a25d4ff828adbf3e54de9d253823be11c1208a
Homann Fritz <fhomann@lcs-mba3118.lcs.on.ca>
2018-01-12 12:51:46 -0500

    making progress

--- Bitmap Image Encoding Part 2.playground/Pages/Decoding an Image.xcplaygroundpage/Contents.swift
+++ Bitmap Image Encoding Part 2.playground/Pages/Decoding an Image.xcplaygroundpage/Contents.swift
@@ -24,17 +24,17 @@ let encodedBitmap = """
 6,6,8
 3,1,2,8,6
 2,2,2,9,5
-1,3,2,9,4
+1,3,2,91
 1,3,2,1,4,5,4
 0,4,8,5,3
-0,4,3,3,3,3,4
+0,4,3,3,3,4,3
 0,4,2,5,2,4,3
 0,4,2,5,2,4,3
 0,4,2,5,2,4,3
 0,4,3,3,3,4,4
 0,5,7,5,4
 1,5,5,5,4
-1,15,4
+1,9,4
 2,13,5
 3,11,6
 5,7,8
@@ -99,6 +99,8 @@ for character in encodedBitmap {
         // Default colour after new line is white
         canvas.fillColor = Color.white
         currentColor = "white"
+        
+    } else if character == "p" {
         
     } else {
         
